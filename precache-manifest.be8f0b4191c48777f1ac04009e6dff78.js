2152b55e3252a4d9d3f269cbd10d26324a63c0c4
Vika Tsukanova <victoriatsukanova@gmail.com>
2019-10-13 01:31:52 -0400

    Updates

---
+++ precache-manifest.be8f0b4191c48777f1ac04009e6dff78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e93aef45accdfe6497fd1c1f2654d61",
+    "url": "/tiff-react-app/index.html"
+  },
+  {
+    "revision": "77e407b8bcd04fdba395",
+    "url": "/tiff-react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3e48a3acb936aace6c57",
+    "url": "/tiff-react-app/static/js/2.8335c16a.chunk.js"
+  },
+  {
+    "revision": "77e407b8bcd04fdba395",
+    "url": "/tiff-react-app/static/js/main.4e34276e.chunk.js"
+  },
+  {
+    "revision": "95776041baed3bba5a8c",
+    "url": "/tiff-react-app/static/js/runtime-main.58a97cbf.js"
+  }
+]);
