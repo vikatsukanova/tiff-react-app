6bcbe463167acc2eae00bf15b3b337983e3d8e50
Vika Tsukanova <victoriatsukanova@gmail.com>
2019-10-13 00:57:10 -0400

    Fetch and display movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import ListItem from './components/ListItem';
 import Toolbar from './components/Toolbar';
+
+const APIKEY = process.env.REACT_APP_API_KEY;
 
 const styles = {
   root: {
@@ -12,17 +14,37 @@ const styles = {
     flex: 1,
     padding: '20px 40px',
   },
+  content: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  }
 }
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const results = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&language=en-US&sort_by=release_date.desc&primary_release_year=2019&vote_count.gte=10`);
+      setData(results.data.results);
+      console.log(results.data.results)
+    };
+    fetchData();
+  }, []);
+
   return (
     <div style={styles.root}>
       <Toolbar />
       <div style={styles.body}>
-        <ListItem
-          imageUrl
-          name="name"
-        />
+        <div style={styles.content}>
+          {data && data.map(movie => (
+            <ListItem
+              key={movie.id} 
+              name={movie.title}
+              imageUrl={movie.poster_path}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,19 +3,28 @@ import React from 'react';
 import Typography from './Typography';
 
 const styles = {
-	listItemContent: {
-	    height: 250,
+	listItem: {
 	    width: 200,
-	    border: '1px solid red',
-	    marginBottom: 10,
+	    margin: 10,
 	},
+	listItemContent: {
+		height: 300,
+		width: 200,
+		overflow: 'hidden',
+		marginBottom: 10,
+	},
+	image: {
+		height: '100%',
+	}
 }
+
+const imageUrl = 'https://image.tmdb.org/t/p/w500'
 
 export default function ListItem(props) {
 	return (
 		<div style={styles.listItem}>
 	        <div style={styles.listItemContent}>
-	        	<img src={props.imageUrl} />
+	        	<img style={styles.image} src={imageUrl+props.imageUrl} alt={''}/>
 	        </div>
 	        <Typography variant="body">{props.name}</Typography>
         </div>
